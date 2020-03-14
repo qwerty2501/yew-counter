85d6c7eba391626da75baa05c6bdfd6a81332947
qwerty2501 <qwerty2501@live.jp>
2020-03-14 21:08:28 +0900

    create initial source code with yew.

---
+++ crate/src/lib.rs
@@ -0,0 +1,29 @@
+mod app;
+#[macro_use]
+extern crate cfg_if;
+
+use wasm_bindgen::prelude::*;
+
+cfg_if! {
+    if #[cfg(feature = "console_error_panic_hook")] {
+        extern crate console_error_panic_hook;
+        use console_error_panic_hook::set_once as set_panic_hook;
+    } else {
+        #[inline]
+        fn set_panic_hook() {}
+    }
+}
+
+cfg_if! {
+    if #[cfg(feature = "wee_alloc")] {
+        extern crate wee_alloc;
+        #[global_allocator]
+        static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+    }
+}
+#[wasm_bindgen]
+pub fn run() -> Result<(), JsValue> {
+    yew::start_app::<app::App>();
+
+    Ok(())
+}
